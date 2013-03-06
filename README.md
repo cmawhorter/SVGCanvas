85268503e10e9df0af661a89e767f617c5977df4
Cory Mawhorter <cory.mawhorter@ephective.com>
2013-03-05 18:18:47 -0800

    first!

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ SVGCanvas
 =========
 
 Updated version of http://svgkit.sourceforge.net/SVGCanvas.html
+
+This project is incomplete and not working.
+
+== Status
+
+This project has stalled until jsdom supports svg.
---
+++ index.js
@@ -0,0 +1,12 @@
+var SVGCanvas = module.exports = require('./lib/SVGCanvas');
+
+ var ctx = new SVGCanvas(200, 200);
+ ctx.beginPath();
+ ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+ ctx.moveTo(110,75);
+ ctx.arc(75,75,35,0,Math.PI,false); // Mouth (clockwise)
+ ctx.moveTo(65,65);
+ ctx.arc(60,65,5,0,Math.PI*2,true); // Left eye
+ ctx.moveTo(95,65);
+ ctx.arc(90,65,5,0,Math.PI*2,true); // Right eye
+ ctx.stroke();
